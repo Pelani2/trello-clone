02ba2d739f24ca0999f97891b973c8e950a285b5
Igor Zupunski <ipatheword@live.com>
2023-07-18 15:47:30 +0200

    app architecture

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import AppRoutes from "./routes/AppRoutes";
+import { RouterProvider } from "react-router-dom";
 
 export default function App() {
   return(
     <>
-      
+      <RouterProvider router={AppRoutes} />
     </>
   )
 }
