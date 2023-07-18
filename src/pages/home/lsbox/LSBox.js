13c5ecfc3afb553cabfee0687c162e9620f92cd8
Igor Zupunski <ipatheword@live.com>
2023-07-18 16:57:41 +0200

    added trello title

--- src/pages/home/lsbox/LSBox.js
+++ src/pages/home/lsbox/LSBox.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./LSBoxStyles.css";
 import LSButton from "../../../components/buttons/lsbutton.js/LSButton";
+import TrelloTitle from "./trello-title/TrelloTitle";
 
 export default function LSBox() {
     return(
         <div className="ls-box">
+            <TrelloTitle />
             <div className="ls-button-container">
                 <LSButton 
                     text="Log In"
---
+++ src/pages/home/lsbox/trello-title/TrelloTitle.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TrelloTitleStyles.css";
+
+export default function TrelloTitle() {
+    return(
+        <h1 className="trello-title">
+            Trello-Clone App
+        </h1>
+    );
+}
