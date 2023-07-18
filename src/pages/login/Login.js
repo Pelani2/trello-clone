66dc43d6d6a198e780b78e3a1eb473730349fcdb
Igor Zupunski <ipatheword@live.com>
2023-07-18 17:24:25 +0200

    added basic login form elements

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,8 +3,40 @@ import "./LoginStyles.css";
 
 export default function Login() {
     return(
-        <div>
-
+        <div className="login-container"> 
+            <h1 className="login-container__title">
+                Log In
+            </h1>
+            <form className="login-form">
+                <div className="login-form__group">
+                    <label 
+                        htmlFor="username"
+                        className="login-form__label"
+                    >
+                        Username:
+                    </label>
+                    <input 
+                        className="login-form__input"
+                        type="text"
+                        id="username"
+                        name="username"
+                    />
+                </div>
+                <div className="login-form__group">
+                    <label 
+                        className="login-form__label"
+                        htmlFor="password"
+                    >
+                        Password:
+                    </label>
+                    <input 
+                        className="login-form__input"
+                        type="password"
+                        id="password"
+                        name="password"
+                    />
+                </div>
+            </form>
         </div>
     )
 }
