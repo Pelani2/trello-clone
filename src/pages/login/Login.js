108fbec099f3d2409c6b7c51c6a9615e891afc4d
Igor Zupunski <ipatheword@live.com>
2023-07-18 17:55:35 +0200

    added the necessary login functions for tracking inputs

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginStyles.css";
 import FormSubmitButton from "../../components/buttons/form-submit-button/FormSubmitButton";
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(`Email: ${email}`);
+        console.log(`Password: ${password}`);
+    };
+
     return(
         <div className="login-container"> 
             <h1 className="login-container__title">
                 Log In
             </h1>
-            <form className="login-form">
+            <form 
+                className="login-form"
+                onSubmit={handleSubmit}
+            >
                 <div className="login-form__group">
                     <label 
                         htmlFor="email"
@@ -21,6 +42,7 @@ export default function Login() {
                         type="text"
                         id="email"
                         name="email"
+                        onChange={handleEmailChange}
                     />
                 </div>
                 <div className="login-form__group">
@@ -35,6 +57,7 @@ export default function Login() {
                         type="password"
                         id="password"
                         name="password"
+                        onChange={handlePasswordChange}
                     />
                 </div>
                 <FormSubmitButton 
